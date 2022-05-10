08d74aa198bfc5bc9274f359f5d85d98f3e824eb
szilagyib <szilagyiborbala8@gmail.com>
2022-05-10 18:40:52 +0200

    Added swagger API, network and ORM layers

--- app/src/main/java/hu/bme/aut/android/recipesearch/di/PersistenceModule.kt
+++ app/src/main/java/hu/bme/aut/android/recipesearch/di/PersistenceModule.kt
@@ -1,10 +1,35 @@
 package hu.bme.aut.android.recipesearch.di
 
+import android.app.Application
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.android.recipesearch.R
+import hu.bme.aut.android.recipesearch.persistence.AppDatabase
+import hu.bme.aut.android.recipesearch.persistence.RecipeDao
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object PersistenceModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(application: Application): AppDatabase {
+        return Room
+            .databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                application.getString(R.string.database)
+            )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRecipeDao(appDatabase: AppDatabase): RecipeDao {
+        return appDatabase.recipeDao()
+    }
 }
--- app/src/main/java/hu/bme/aut/android/recipesearch/ui/main/MainRepository.kt
+++ app/src/main/java/hu/bme/aut/android/recipesearch/ui/main/MainRepository.kt
@@ -1,4 +1,17 @@
 package hu.bme.aut.android.recipesearch.ui.main
 
-class MainRepository {
+import hu.bme.aut.android.recipesearch.model.Recipe
+import hu.bme.aut.android.recipesearch.network.RecipeService
+import hu.bme.aut.android.recipesearch.persistence.RecipeDao
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    private val recipeService: RecipeService,
+    private val recipeDao: RecipeDao
+) {
+    suspend fun loadRecipes() {
+        val recipeList: List<Recipe> = recipeDao.getRecipeList()
+        if (recipeList.isEmpty())
+            recipeDao.insertRecipeList(recipeService.fetchRecipeList())
+    }
 }
