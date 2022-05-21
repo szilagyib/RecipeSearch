8a6b8c678ec63e663c47d5bc6d2f2155afbf5d3d
szilagyib <szilagyiborbala8@gmail.com>
2022-05-21 18:44:29 +0200

    UI design with compose

--- app/src/main/java/hu/bme/aut/android/recipesearch/ui/main/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/android/recipesearch/ui/main/MainActivity.kt
@@ -1,10 +1,27 @@
 package hu.bme.aut.android.recipesearch.ui.main
 
+import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import hu.bme.aut.android.recipesearch.model.Recipe
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     internal val viewModel: MainViewModel by viewModels()
+
+    private val testList = mutableListOf<Recipe>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //testlist init
+        for(i in 0..20)
+            testList.add(Recipe.mock())
+
+        super.onCreate(savedInstanceState)
+        setContent {
+            //RecipeList(testList, Navigation.)
+            Navigation(testList)
+        }
+    }
 }
